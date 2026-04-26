2ff73c94dcb0cc3f96c812fe3ef5ffdd82f71ad0
jannacrisshorsts-rgb <jannacrisshorsts@gmail.com>
2026-04-26 08:34:14 -0300

    Add files via upload

---
+++ iniciar_jogo.bat
@@ -0,0 +1,10 @@
+@echo off
+echo.
+echo ========================================
+echo Shadow Duel Arena - Servidor Local
+echo ========================================
+echo.
+
+python server.py
+
+pause
